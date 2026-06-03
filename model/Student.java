773acfde565ea81904fc61ffb96438ae3c9f6689
Prayas248 <prayasyadav248@gmail.com>
2026-06-03 14:16:13 +0530

    create student model

--- model/Student.java
+++ model/Student.java
@@ -1,4 +1,16 @@
 package model;
 
+import lombok.*;
+
+// model for student containing all the fields involved in Student table
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Student {
+    private Long id;
+    private String name;
+    private String course;
+    private String email;
+    private String phone;
+    private boolean is_active;
 }
