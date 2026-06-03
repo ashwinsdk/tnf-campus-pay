7dc175e5f8e59c49e92d8b21496837c17d5b3db8
Shashank Kumar Lal <shashanklal309@gmail.com>
2026-06-03 16:32:20 +0530

    logging implemented1.1

--- config/DatabaseConfig.java
+++ config/DatabaseConfig.java
@@ -1,21 +1,27 @@
 package config;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConfig{
+public class DatabaseConfig {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/tnfcampus_pay";
-    private static final String USER = "root";
+
+    private static final String URL = "jdbc:mysql://localhost:3306/campus_pay";
+    private static final String USERNAME = "root";
     private static final String PASSWORD = "";
 
-    public static Connection getConnection() throws SQLException {
+    static {
+        try {
 
-        return DriverManager.getConnection(
-                URL,
-                USER,
-                PASSWORD
-        );
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("CRITICAL: MySQL JDBC Driver missing! Check your buildpath/dependencies.");
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
