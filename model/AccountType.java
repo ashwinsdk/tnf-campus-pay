ffde79220e88a81b5f3ef1b71addd02c8ac244f3
ashwinsdk <ashwin2005s@gmail.com>
2026-06-03 15:58:32 +0530

    wallet implemenented

--- model/AccountType.java
+++ model/AccountType.java
@@ -2,13 +2,16 @@ package model;
 
 import lombok.Data;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @Data
-public abstract class Wallet {
+public abstract class AccountType {
     private int walletId;
     private int studentId;
     private double balance;
 
-    public abstract void deposit(double amount);
-    public abstract void withdraw(double amount );
-    public abstract void transfer(int fromWalletId, int toWalletId, double amount);
+    public abstract void deposit(Connection conn, double amount, int toId) throws SQLException;
+    public abstract void withdraw(Connection conn, double amount, int fromId) throws SQLException;
+    public abstract void transfer(Connection conn, int fromId, int toId, double amount) throws SQLException;
 }
--- service/Wallet.java
+++ service/Wallet.java
@@ -1,26 +1,49 @@
-package model;
+package service;
 
+import config.DatabaseConfig;
+import model.AccountType;
+import repository.WalletRepository;
 import util.InputValidator;
+
+import java.sql.*;
 
 public class Wallet extends AccountType {
     InputValidator input = new InputValidator();
+    WalletRepository walletRepo;
+    public Wallet(){
+        walletRepo = new WalletRepository();
 
+    }
     @Override
-    public void deposit(double amount) {
-        if(input.checkInput(amount)) {
-            setBalance(getBalance() + amount);
+    public void deposit(Connection conn, double amount, int toId) throws SQLException {
+        if (input.checkInput(amount)){
+            walletRepo.depositIntoDB(conn,amount,toId);
+            System.out.println("[SUCCESS] " + amount + " Deposited");
         }
+        else {
+            System.out.println("[ERROR] Deposit failed !");
+        }
+
+
     }
 
     @Override
-    public void withdraw(double amount) {
-        if (input.checkInput(amount) && amount >= getBalance()){
-            setBalance(getBalance() - amount);
+    public void withdraw(Connection conn, double amount, int fromId) throws SQLException {
+
+        double balance = walletRepo.getBalanceFromWallet(conn, fromId);
+
+        if (balance >= amount){
+            walletRepo.withdrawFromDB(conn, amount, fromId);
         }
+        else {
+            System.out.println("[ERROR] Deposit failed !");
+        }
+
     }
 
     @Override
-    public void transfer(int fromWalletId, int toWalletId, double amount) {
-
+    public void transfer(Connection conn, int fromId, int toId, double amount) throws SQLException {
+        withdraw(conn, amount, fromId);
+        deposit(conn, amount, toId);
     }
 }
