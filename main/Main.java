eabc5f25ada926c26b6d683b9da969d7d1669e6f
Ashwin Sudhakar <ashwin2005s@gmail.com>
2026-06-03 18:00:35 +0530

    Comment out logger and student registration
    Comment out logger and student registration code.

--- main/Main.java
+++ main/Main.java
@@ -9,13 +9,13 @@ import service.StudentServiceImplementation;
 import java.sql.SQLException;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    // private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws SQLException {
-        Student st = new Student("Dilip Choubey","BTech CSE","dilip@gmail.com","1224567890");
+        // Student st = new Student("Dilip Choubey","BTech CSE","dilip@gmail.com","1224567890");
       //  StudentRepositoryImplementation std = new StudentRepositoryImplementation();
-        StudentServiceImplementation studentService = new StudentServiceImplementation();
+        // StudentServiceImplementation studentService = new StudentServiceImplementation();
 
-        studentService.registerStudent(st);
+        // studentService.registerStudent(st);
        // System.out.println(stud);
       //  std.findByEmail("prayas@example.com");
       //  List<Student> studentList = std.findAll();
