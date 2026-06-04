c3268961a94f449ead9f8f277d9f80066c85d2b9
Ashwin Sudhakar <ashwin2005s@gmail.com>
2026-06-04 09:29:31 +0530

    Comment out payment processing logic
    Comment out payment-related code for testing purposes.

--- main/Main.java
+++ main/Main.java
@@ -11,14 +11,14 @@ public class Main {
 
     public static void main(String[] args) throws  Exception{
 
-        Connection connection = DatabaseConfig.getConnection();
-        InstituteMoney im = new InstituteMoney();
-        CampusPayment cm = new WorkShop();
-        CampusPaymentService cp = new CampusPaymentService();
-      //  cp.payFine(im,cm,connection,1,"TRANSFER");
-        StudentServiceImplementation st = new StudentServiceImplementation();
-        Student stud = st.getStudentById(connection,2);
-        cp.payFee(stud,cm,connection);
-        System.out.println("payFine called");
+      //   Connection connection = DatabaseConfig.getConnection();
+      //   InstituteMoney im = new InstituteMoney();
+      //   CampusPayment cm = new WorkShop();
+      //   CampusPaymentService cp = new CampusPaymentService();
+      // //  cp.payFine(im,cm,connection,1,"TRANSFER");
+      //   StudentServiceImplementation st = new StudentServiceImplementation();
+      //   Student stud = st.getStudentById(connection,2);
+      //   cp.payFee(stud,cm,connection);
+      //   System.out.println("payFine called");
     }
 }
